1d519f1777179418a150ff03578fd7108b477aa9
Stephen Planck <splanck9271@gmail.com>
2020-12-15 20:04:55 +1000

    Got a booting kernel that handles the timer interrupt.

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include "monitor.h"
+#include "descriptor_tables.h"
+#include "timer.h"
+
+int main(struct multiboot *mboot_ptr)
+{
+    init_descriptor_tables();
+    
+    monitor_clear();
+    monitor_write("Kernel starting...\n\n");
+
+    asm volatile("int $0x3");
+    asm volatile("int $0x4");
+    monitor_write("Sending sti instruction...\n");
+
+    asm volatile("sti");
+    monitor_write("sti command sent...\n");
+
+    init_timer(50); // Initialise timer to 50Hz 
+    monitor_write("Timer initialised.\n\n");
+
+    for(;;) 
+    {
+        asm("hlt");
+    } 
+
+    return 0;
+}
