2e58cad68b26bfbb9676afbe9ba9331e067e9b98
Stephen Planck <splanck9271@gmail.com>
2020-12-19 20:44:17 +1000

    Started implementing memory paging, but not working yet.

--- src/common.h
+++ src/common.h
@@ -22,4 +22,10 @@ int strcmp(char *str1, char *str2);
 char *strcpy(char *dest, const char *src);
 char *strcat(char *dest, const char *src);
 
+#define PANIC(msg) panic(msg, __FILE__, __LINE__);
+#define ASSERT(b) ((b) ? (void)0 : panic_assert(__FILE__, __LINE__, #b))
+
+extern void panic(const char *message, const char *file, u32int line);
+extern void panic_assert(const char *file, u32int line, const char *desc);
+
 #endif // COMMON_H
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "monitor.h"
 #include "descriptor_tables.h"
 #include "timer.h"
+#include "paging.h"
 
 int main(struct multiboot *mboot_ptr)
 {
@@ -18,6 +19,12 @@ int main(struct multiboot *mboot_ptr)
 
     init_timer(50); // Initialise timer to 50Hz 
     monitor_write("Timer initialised.\n\n");
+
+    initialise_paging();
+    monitor_write("Hello, paging world!\n");
+
+    //u32int *ptr = (u32int*)0xA0000000;
+    //u32int do_page_fault = *ptr;
 
     for(;;) 
     {
